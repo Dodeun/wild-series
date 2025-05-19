4065b9ab38148dba1863af300927044ab3121661
Burdon Jordan <burdon.jordan@gmail.com>
2025-05-19 15:54:32 +0200

    step4

---
+++ server/src/modules/category/categoryActions.ts
@@ -0,0 +1,45 @@
+// Some data to make the trick
+
+const categories = [
+  {
+    id: 1,
+
+    name: "Comédie",
+  },
+
+  {
+    id: 2,
+
+    name: "Science-Fiction",
+  },
+];
+
+import type { RequestHandler } from "express";
+
+// Declare the actions
+
+const browse: RequestHandler = (req, res) => {
+  if (req.query.q != null) {
+    const filteredCategories = categories.filter((category) =>
+      category.name.includes(req.query.q as string),
+    );
+    res.json(filteredCategories);
+  } else {
+    res.json(categories);
+  }
+};
+
+const read: RequestHandler = (req, res) => {
+  const parsedId = Number.parseInt(req.params.id);
+
+  const category = categories.find((category) => category.id === parsedId);
+
+  if (category != null) {
+    res.json(category);
+  } else {
+    res.sendStatus(404);
+  }
+};
+// Export them to import them somewhere else
+
+export default { browse, read };
